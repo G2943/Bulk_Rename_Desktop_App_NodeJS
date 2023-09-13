b05a6afe6a8dd9bfcedfa6e75dd35bb90efc17c0
Vaibhav Narayan Gole <vaibhavgole88@gmail.com>
2023-09-13 13:31:36 +0530

    Bulk_Rename_Feature_Completed_For_Desktop_Using_NodeJS

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const fs = require('fs');
 
+const replaceThis = "Reva";
+const replaceWith = "reva";
+
+const preview = true;
+
 try {
   fs.readdir("data", (err, data)=>{
     console.log(data);
+    for (let index = 0; index < data.length; index++) {
+      const item = data[index];
+      let newFile = "data/" + item.replaceAll(replaceThis, replaceWith);
+
+      if(!preview){
+        fs.rename("data/" + item, newFile, ()=>{
+          console.log("Rename Success")
+        })
+      }
+      else{
+        if("data/" + item !== newFile){
+          console.log("data/"+item + " will be renamed to " + newFile)
+          
+        } 
+      }
+    }
   })
  } catch (err) {
   console.error(err);
